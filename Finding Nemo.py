4b38a2dda6077ca57d3f8a956eb8394e9f8704bc
NotGonnaUseThisMuch <113238211+NotGonnaUseThisMuch@users.noreply.github.com>
2022-10-08 11:00:06 +0100

    Add files via upload

---
+++ Finding Nemo.py
@@ -0,0 +1,19 @@
+story ="""Nemo is a young clownfish who lives with his father Marlin in an anemone in the Great Barrier Reef. 
+Nemo, despite being hampered by a lame right fin, is eager to explore life around the ocean. Marlin
+however is overprotective of him, having lost his wife Coral and all their other eggs in a barracuda attack, 
+leaving Nemo as his only child. On Nemo's first day of school, Marlin unintentionally embarrasses him, and while Marlin 
+is distracted with the teacher, Mr. Ray, Nemo defiantly sneaks away from the reef toward a speedboat, where he is captured 
+by a pair of scuba divers. Marlin tries to chase the boat and meets Dory, a blue tang who suffers from acute short-term memory 
+loss, who offers her help. During an encounter with three sharks who have sworn to abstain from eating other fish, Marlin 
+notices a diver's mask that fell from the boat that took Nemo. Marlin and Dory fight over the mask, giving Dory a 
+nose bleed, which sends one of the sharks into a feeding frenzy, and Marlin and Dory narrowly escape, but are knocked 
+unconscious when the sharks accidentally set off a ring of old naval mines."""
+
+word = input("What word would you like to find in the story?")
+def search(story, word):
+    if (story.find(word) == -1):
+        print("Word not found ._.")
+    else:
+        print("The word was found at", story.find(word))
+        print("number of times string found is", story.count(word))
+search(story,word)
